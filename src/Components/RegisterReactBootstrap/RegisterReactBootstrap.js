ff0bbe7af54eb157da0fd15f722c0c8f3f19dda6
raSel <mdraselm413@gmail.com>
2022-10-14 18:43:16 +0600

    validation

--- src/Components/RegisterReactBootstrap/RegisterReactBootstrap.js
+++ src/Components/RegisterReactBootstrap/RegisterReactBootstrap.js
@@ -30,12 +30,11 @@ const handleRegister = (event) =>{
       <Form onSubmit={handleRegister}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control name="email" type="email" placeholder="Enter email" />
+          <Form.Control name="email" type="email" placeholder="Enter email" required />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control name="password" type="password" placeholder="Password" />
+          <Form.Control name="password" type="password" placeholder="Password" required/>
         </Form.Group>
         <Button variant="primary" type="submit">
           Register
