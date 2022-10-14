82a05fd3c69ea9e464fb9f25821dfbb58da59fd2
raSel <mdraselm413@gmail.com>
2022-10-14 20:43:14 +0600

    reload

--- src/Components/LoginBootstrap/LoginBootstrap.js
+++ src/Components/LoginBootstrap/LoginBootstrap.js
@@ -1,14 +1,29 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
+import app from "../../firebase/firebase.init";
+
+const auth = getAuth(app)
 
 const LoginBootstrap = () => {
     const handleSubmit = event =>{
         event.preventDefault()
         const form = event.target;
-        const email = form.email;
-        const password = form.password;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        signInWithEmailAndPassword(auth,email,password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error =>{
+            console.error(error);
+        })
+
     }
   return (
     <div className="w-50 mx-auto">
