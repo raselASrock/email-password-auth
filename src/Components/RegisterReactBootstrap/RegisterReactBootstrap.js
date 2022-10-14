357b3ca0fe945910f24b393ed0e22242cf3c0158
raSel <mdraselm413@gmail.com>
2022-10-14 18:34:28 +0600

    firebase profile

--- src/Components/RegisterReactBootstrap/RegisterReactBootstrap.js
+++ src/Components/RegisterReactBootstrap/RegisterReactBootstrap.js
@@ -1,12 +1,27 @@
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import app from "../../firebase/firebase.init";
+
+const auth = getAuth(app);
+
+
+
 const RegisterReactBootstrap = () => {
 const handleRegister = (event) =>{
   event.preventDefault();
   const email = event.target.email.value;
   const password = event.target.password.value;
   console.log(email, password);
+  createUserWithEmailAndPassword(auth, email, password)
+  .then( result =>{
+    const user = result.user
+    console.log(user)
+  })
+  .catch(error =>{
+    console.error(error);
+  })
 }
 
   return (
