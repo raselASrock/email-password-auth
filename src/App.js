1cf5aadd0e16f337f895bb5b29d17bb9a4fe92a9
raSel <mdraselm413@gmail.com>
2022-10-14 11:21:15 +0600

    pass

--- src/App.js
+++ src/App.js
@@ -5,13 +5,22 @@ import app from './firebase/firebase.init';
 
 
 const auth = getAuth(app); 
+
+const handleSignUp = (event) =>{
+  event.preventDefault();
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+  console.log(event.target.password.value);
+}
 function App() {
   return (
     <div className="App">
-      <form>
-        <input type="email" name="" id="" placeholder='Email-Please' />
+      <form onSubmit={handleSignUp}>
+        <input type="email" name="email" id="" placeholder='Email-Please' />
         <br />
-        <input type="password" name="" id="" placeholder='Password-Here' />
+        <input type="password" name="password" id="" placeholder='Password-Here' />
+        <br/>
+        <button type='submit'>Sign Up</button>
       </form>
     </div>
   );
