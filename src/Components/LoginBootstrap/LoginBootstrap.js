a1bc0d9befee36563114b0750e29df8dec93598d
raSel <mdraselm413@gmail.com>
2022-10-15 19:56:59 +0600

    final

--- src/Components/LoginBootstrap/LoginBootstrap.js
+++ src/Components/LoginBootstrap/LoginBootstrap.js
@@ -1,5 +1,8 @@
-import { Toast } from "bootstrap";
-import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import React from "react";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
@@ -7,79 +10,84 @@ import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 import app from "../../firebase/firebase.init";
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 const LoginBootstrap = () => {
-  const [success, setSuccess] = useState(false)
-  const [userEmail, setUserEmail] = useState('')
-    const handleSubmit = event =>{
-        event.preventDefault()
-        setSuccess(false)
-        const form = event.target;
-        
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password);
+  const [success, setSuccess] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
 
-        signInWithEmailAndPassword(auth,email,password)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
-            setSuccess(true)
-        })
-        .catch(error =>{
-            console.error(error);
-        })
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSuccess(false);
+    const form = event.target;
 
-    const handleEmailBlur = event =>{
-      const email = event.target.value;
-      setUserEmail(email);
-      console.log(email);
-    }
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
 
-
-    const handleForgetPassword = () =>{
-      if(!userEmail){
-        alert('Please Enter your email')
-      }
-      sendPasswordResetEmail(auth, userEmail)
-      .then(() =>{
-        alert('Reset Password Sent') 
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setSuccess(true);
       })
-      .catch( error =>{
+      .catch((error) => {
         console.error(error);
-      })
-    }
+      });
+  };
 
+  const handleForgetPassword = () => {
+    if (!userEmail) {
+      alert("Please Enter your email");
+    }
+    sendPasswordResetEmail(auth, userEmail)
+      .then(() => {
+        alert("Reset Password Sent");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <div className="w-50 mx-auto">
       <h3 className="text-primary">Please Log In</h3>
       <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicName">
-          <Form.Label>Your Name</Form.Label>
-          <Form.Control onBlur={handleEmailBlur} name="name" type="text" placeholder="Enter Your Name" />
-          <Form.Text className="text-muted">
-            We'll never share your Data with anyone else.
-          </Form.Text>
-        </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control onBlur={handleEmailBlur} name="email" type="email" placeholder="Enter email" />
+          <Form.Control name="email" type="email" placeholder="Enter email" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control name="password" type="password" placeholder="Password" />
+          <Form.Control
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Login
         </Button>
       </Form>
-      { success && <p className="text-success">Successfully login to the account</p>}
-      <p><small>New to this Website? Please <Link to='/register'>Register</Link></small></p>
-      <p>Forget Password? <button className="btn btn-link" onClick={handleForgetPassword} type="button">Please Reseat</button></p>
+      {success && (
+        <p className="text-success">Successfully login to the account</p>
+      )}
+      <p>
+        <small>
+          New to this Website? Please <Link to="/register">Register</Link>
+        </small>
+      </p>
+      <p>
+        Forget Password?{" "}
+        <button
+          className="btn btn-link"
+          onClick={handleForgetPassword}
+          type="button"
+        >
+          Please Reseat
+        </button>
+      </p>
     </div>
   );
 };
--- src/Components/RegisterReactBootstrap/RegisterReactBootstrap.js
+++ src/Components/RegisterReactBootstrap/RegisterReactBootstrap.js
@@ -1,4 +1,9 @@
-import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+  updateProfile,
+} from "firebase/auth";
 import React from "react";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
@@ -8,98 +13,113 @@ import app from "../../firebase/firebase.init";
 
 const auth = getAuth(app);
 
-
-
 const RegisterReactBootstrap = () => {
-  const [passwordError, setPasswordError] = useState('');
-  const [success, setSucces] = useState(false)
-  const [userEmail, setUserEmail] = useState('')
-const handleRegister = (event) =>{
+  const [passwordError, setPasswordError] = useState("");
+  const [success, setSucces] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
 
-  event.preventDefault();
-  setSucces(false)
-  const form = event.target;
-  const name = form.name.value;
-  const email = form.email.value;
-  const password = form.password.value;
-  console.log(name, email, password);
-  // if(!/(?=.*[A-Z].*[A-Z])/.test(password)){
-  //   setPasswordError('Please provide at least two uppercase');
-  //   return;
-  // }
-  if(password.length < 6){
-    setPasswordError('Please provide at least six character');
-    return;
-  }
-  if(!/(?=.*[!@#$%&*])/.test(password)){
-    setPasswordError("Please add at least one special character.")
-    return;
-  }
-  setPasswordError('')
-  createUserWithEmailAndPassword(auth, email, password)
-  .then( result =>{
-    const user = result.user
-    console.log(user)
-    setSucces(true)
-    form.reset();
-    verifyEmail();
-    updateUserName(name);
-  })
-  .catch(error =>{
-    console.error(error);
-    setPasswordError(error.message)
-  })
-}
+  const handleRegister = (event) => {
+    event.preventDefault();
+    setSucces(false);
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, email, password);
+    if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+      setPasswordError("Please provide at least two uppercase");
+      return;
+    }
+    if (password.length < 6) {
+      setPasswordError("Please provide at least six character");
+      return;
+    }
+    if (!/(?=.*[!@#$%&*])/.test(password)) {
+      setPasswordError("Please add at least one special character.");
+      return;
+    }
+    setPasswordError("");
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setSucces(true);
+        form.reset();
+        verifyEmail();
+        updateUserName(name);
+      })
+      .catch((error) => {
+        console.error(error);
+        setPasswordError(error.message);
+      });
+  };
 
-const verifyEmail = () =>{
-  sendEmailVerification(auth.currentUser)
-  .then( () =>{
-    alert('Please Check Your Email.')
-  })
-}
-const handleEmailBlur = event =>{
-  const email = event.target.value;
-  setUserEmail(email);
-  console.log(email);
-}
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser).then(() => {
+      alert("Please Check Your Email.");
+    });
+  };
+  const handleEmailBlur = (event) => {
+    const email = event.target.value;
+    setUserEmail(email);
+    console.log(email);
+  };
 
-  const updateUserName = (name) =>{
+  const updateUserName = (name) => {
     updateProfile(auth.currentUser, {
-      displayName: name
+      displayName: name,
     })
-    .then(() =>{
-      console.log('Display Name Updated');
-    })
-    .catch(error => console.error(error))
-  }
-
+      .then(() => {
+        console.log("Display Name Updated");
+      })
+      .catch((error) => console.error(error));
+  };
 
   return (
     <div className="w-50 mx-auto">
       <h3 className="text-primary">Please Register</h3>
       <Form onSubmit={handleRegister}>
-      <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Your Name</Form.Label>
-          <Form.Control onBlur={handleEmailBlur} name="name" type="text" placeholder="Enter Your Name" />
+          <Form.Control
+            onBlur={handleEmailBlur}
+            name="name"
+            type="text"
+            placeholder="Enter Your Name"
+          />
           <Form.Text className="text-muted">
             We'll never share your Data with anyone else.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control name="email" type="email" placeholder="Enter email" required />
+          <Form.Control
+            name="email"
+            type="email"
+            placeholder="Enter email"
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control name="password" type="password" placeholder="Password" required/>
+          <Form.Control
+            name="password"
+            type="password"
+            placeholder="Password"
+            required
+          />
         </Form.Group>
         <p className="text-danger">{passwordError}</p>
-        {success && <p className="text-success">User Created Successfully</p> }
+        {success && <p className="text-success">User Created Successfully</p>}
         <Button variant="primary" type="submit">
           Register
         </Button>
       </Form>
-      <p><small>Already have an Account? Please <Link to='/login'>Login</Link></small></p>
+      <p>
+        <small>
+          Already have an Account? Please <Link to="/login">Login</Link>
+        </small>
+      </p>
     </div>
   );
 };
